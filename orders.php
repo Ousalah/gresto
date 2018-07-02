e8c6ee3c3376c0e3a4ca57d69ddc029a6a5306d1
Mohamed Ousalah <mohamed@ousalah.com>
2018-07-02 10:42:28 +0100

    feat: add orders details page

---
+++ orders.php
@@ -0,0 +1,270 @@
+<?php require_once("fragments/header.php"); ?>
+
+    <!-- Start Content -->
+    <div class="orders content">
+		<div class="container">
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+			<div class="order-detail">
+				<div class="row">
+					<div class="col-xs-3 col-sm-2">
+						<img class="img-responsive img-thumbnail" src="https://picsum.photos/0/100/?random" alt="">
+					</div>
+					<div class="col-xs-8 col-sm-9">
+						<div class="row">
+							<div class="col-xs-12 col-sm-7">
+								<span class="order-title">Product title without description</span>
+							</div>
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">10 DH</span>
+							</div>
+							<div class="col-xs-4 col-sm-1">
+								<input type="text" class="form-control order-qte" value="1">
+							</div><!-- 
+							<div class="col-xs-4 col-sm-2">
+								<span class="order-price">20 DH</span>
+							</div> -->
+						</div>
+					</div>
+					<div class="col-xs-1 col-sm-1">
+						<a class="order-delete" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+		    </div>
+		</div>
+	</div>
+    <!-- End Content -->
+	
+<?php require_once("fragments/footer.php"); ?>
